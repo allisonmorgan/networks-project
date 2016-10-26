3b9725748a422cc36a63862c7b9aa5fc84b3a6a1
dimecon <deconomou2008@gmail.com>
2016-10-25 19:33:11 -0600

    silly pep8 fixes

--- epidemic.py
+++ epidemic.py
@@ -1,8 +1,10 @@
 import networkx as nx
 import random
 
+
 def flip(p):
     return True if random.random() <= p else False
+
 
 class SI:
     """An SI epidemic model.
@@ -15,26 +17,26 @@ class SI:
 
     """
     def __init__(self, graph, p=0.5):
-        self.p           = p
-        self.graph       = graph
+        self.p = p
+        self.graph = graph
         self.susceptible = set(nx.nodes(graph))
-        self.infected    = set()
-        self.time        = 0
+        self.infected = set()
+        self.time = 0
         self.is_complete = False
-        self.visited     = set()
+        self.visited = set()
 
     def __step(self):
         is_complete = True
         for u in self.infected.copy():
             nbrs = [v for v in self.graph.neighbors(u)
-                    if v in self.susceptible and (u,v) not in self.visited]
+                    if v in self.susceptible and (u, v) not in self.visited]
             for v in nbrs:
                 if flip(self.p):
                     self.susceptible.remove(v)
                     self.infected.add(v)
                     is_complete = False
                 else:
-                    self.visited.add((u,v))
+                    self.visited.add((u, v))
         self.is_complete = is_complete
         if not self.is_complete:
             self.time += 1
