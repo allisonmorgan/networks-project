c48128c8743e0569aacf905b81d4f3195702fa96
dimecon <deconomou2008@gmail.com>
2016-11-04 00:22:18 -0600

    initial incremental averaging

--- facultyexample.py
+++ facultyexample.py
@@ -39,6 +39,43 @@ def main():
     plt.xlabel('University prestige (pi)')
     plt.ylabel('Epidemic size')
     plt.savefig('results/size-{}-trials-{}-p.png'.format(n_trials, p))
+    
+    # Start of incremental averaging (haven't throughly checked if correct).
+    #trial_number = 1
+    #results_length = defaultdict(int)
+    #results_size = defaultdict(int)
+    #p = 0.1
+    #while True:
+    #    print(trial_number)
+    #    for node in school_metadata.keys():
+    #        #print(node / float(faculty_graph.number_of_nodes()))
+    #        epi = SI(faculty_graph.copy(), p=p)
+    #        epi.infect_node(node)
+    #        epi.simulate()
+    #        prev_avg_length = results_length[school_metadata[node]["pi"]]
+    #        prev_avg_size = results_size[school_metadata[node]["pi"]]
+             # http://math.stackexchange.com/questions/106700/incremental-averageing
+    #        results_length[school_metadata[node]["pi"]] = prev_avg_length + \
+    #            (epi.length - prev_avg_length) / float(trial_number)
+    #        results_size[school_metadata[node]["pi"]] = prev_avg_size + \
+    #            (epi.size - prev_avg_size) / float(trial_number)
+
+    #    if np.mod(trial_number, 5) == 0:
+    #        ax = plt.gca()
+    #        ax.scatter(*zip(*results_length.items()))
+    #        plt.xlabel('University prestige (pi)')
+    #        plt.ylabel('Epidemic length')
+    #        plt.savefig('results/length-{}-trials-{}-p.png'.format(trial_number, p))
+    #        plt.clf()
+
+    #        ax = plt.gca()
+    #        ax.scatter(*zip(*sorted(results_size.items(), key=lambda x: x[0])))
+    #        plt.xlabel('University prestige (pi)')
+    #        plt.ylabel('Epidemic size')
+    #        plt.savefig('results/size-{}-trials-{}-p.png'.format(trial_number, p))
+    #        plt.clf()
+
+    #    trial_number += 1
 
 
 if __name__ == "__main__":
