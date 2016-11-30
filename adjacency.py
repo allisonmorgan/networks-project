99d7c688c89973db9e3e4a07c9e4f9044344e2a0
Allison Morgan <allison.morgan@gmail.com>
2016-11-30 14:52:32 -0700

    Plot adjacency matrices

--- adjacency.py
+++ adjacency.py
@@ -1,21 +1,71 @@
+
 import networkx as nx
 import matplotlib.pyplot as plt
-from importfaculty import faculty_graph
+import matplotlib.ticker as ticker
+import matplotlib.cm as cm
 import numpy as np
 
+from importcompsci import school_metadata as meta_cs, faculty_graph as g_cs
+from importhistory import school_metadata as meta_his, faculty_graph as g_his
+from importbusiness import school_metadata as meta_busi, faculty_graph as g_busi
+
 def main():
+  fig, axarray = plt.subplots(1, 3, figsize=(15,5))
+  #fig = plt.figure(figsize=(15,10))
 
-  # Vertices are ordered by prestige in the dataset
-  adj = nx.to_numpy_matrix(faculty_graph, dtype=float)
+  for l, (g, title) in enumerate([(g_busi, "Business"), (g_cs, "Computer Science"), (g_his, "History")]):
+    # Vertices are ordered by prestige in the dataset
+    adj = nx.to_numpy_matrix(g, dtype=int)
 
-  # Scale adjacency matrix by a vertex's outdegree.
-  # Edges i->j are from row_i -> col_j
-  for i, row in enumerate(adj):
-    scaled = []
-    for j, cell in enumerate(row[0]):
-      scaled.append(cell/float(faculty_graph.in_degree(j+1)))
-    
-    adj[i] = scaled
+    #fig = plt.figure(figsize=(15,10))
+    #ax = fig.add_subplot(111)
+    #cax = plt.matshow(adj, cmap=plt.cm.Blues)
+    #fig.colorbar(cax)
 
-  plt.matshow(adj, cmap=plt.cm.Blues)
-  plt.show()
+    #ax.set_ylabel("PhD Granting Institution")
+    #ax.set_xlabel("Faculty Placement Institution")
+
+    #plt.savefig("results/adjacency.png")
+    #plt.clf()
+
+    # Scale adjacency matrix by a vertex's outdegree.
+    # Edges i -> j are from row_i -> col_j
+    groups = np.linspace(0, 100, 11, dtype=int)
+    grouped_by_row = []
+    for i, row in enumerate(adj):
+      in_edges = [] 
+      for rank, edges in enumerate(row[0].tolist()[0]):
+        for j in range(int(edges)):
+          in_edges.append(rank)
+      grouped_row, _ = np.histogram(in_edges, groups)
+      grouped_by_row.append(grouped_row)
+
+    grouped = [np.zeros(len(groups)-1) for i in range(len(groups)-1)]
+    for i, row in enumerate(grouped_by_row):
+      for j in range(len(groups)-1):
+        if i <= groups[j+1]:
+          for k, elem in enumerate(row):
+            grouped[j][k] += elem
+          break
+
+    #ax = fig.add_subplot(111)
+    cax = axarray[l].matshow(grouped, cmap=cm.gray_r)
+
+    axarray[l].xaxis.set_major_locator(ticker.MultipleLocator(1))
+    axarray[l].yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    axarray[l].set_xticklabels(groups)
+    axarray[l].set_yticklabels(groups)
+
+    if l == 0:
+      axarray[l].set_ylabel("PhD Granting Institution")
+      axarray[l].set_xlabel("Faculty Placement Institution")
+
+    axarray[l].set_title(title)
+
+  #fig.colorbar(cax)
+
+  plt.savefig("results/grouped_adjacency.png")
+
+if __name__ == "__main__":
+    main()
